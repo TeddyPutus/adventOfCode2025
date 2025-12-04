f1324c1e617b0e1a620bd20d9c28493fb696a684
teddy <teddyputus1@gmail.com>
2025-12-04 21:49:31 +0000

    Day 2 part 1 DONE in C

--- src/day2/main.c
+++ src/day2/main.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 // #include <regex.h> - not supported in windows :'( IF ONLY!
 // regex expression would be "(\d+)(\1)"
+
+typedef unsigned long long int BigNumber;
 
 // const char relativePath[] = "../../input_files/day_2_example.txt";
 const char relativePath[] = "../../input_files/day_2.txt";
@@ -17,36 +20,50 @@ void raiseError(char *message){
     exit(1);
 }
 
-unsigned long long int getInvalidIdCount(char *buffer[2]) {
-    int startNumber = atoi(buffer[0]);
-    int endNumber = atoi(buffer[1]);
+int numPlaces (const BigNumber n) {
+    if (n < 10) return 1;
+    return 1 + numPlaces (n / 10);
+}
 
-    unsigned long long int invalidIdCount = 0;
+BigNumber  getDigit(BigNumber  number, int digit){
+    return number / (BigNumber) pow(10, digit) % 10;
+}
 
-    for (int i = startNumber; i <= endNumber; i++) {
-        char id[MAX_STRING_LEN];
-        sprintf(id, "%d", i);
+BigNumber extractNumber(BigNumber id, int digitCount) {
+    BigNumber result = 0;
+    for (int x = 1; x <= digitCount/2; x++) {
+        result = result * 10 + getDigit(id, digitCount - x);
+    }
+    return result;
+}
 
-        if (strlen(id) % 2 == 0) {
-            int halfLength = strlen(id) / 2;
-            char firstHalf[halfLength + 1];
-            char secondHalf[halfLength + 1];
-            strncpy(firstHalf, id, halfLength);
-            strncpy(secondHalf, id + halfLength, halfLength);
-            firstHalf[halfLength] = '\0';
-            secondHalf[halfLength] = '\0';
+BigNumber rebuildNumber(BigNumber value, int digitCount) {
+    for (int x = 1; x <= digitCount/2; x++) {
+        value = value * 10;
+    }
+    return value;
+}
 
-            if (atoi(firstHalf) == atoi(secondHalf)) {
-                // printf("InvalidId: %s\n", id);
-                invalidIdCount+= i;
-            }
-        }
+BigNumber getInvalidIdCount(char *buffer[2]) {
+    BigNumber startNumber = atoll(buffer[0]);
+    BigNumber endNumber = atoll(buffer[1]);
 
+    BigNumber invalidIdCount = 0;
+
+    for (BigNumber i = startNumber; i <= endNumber; i++) {
+        int digitCount = numPlaces(i);
+        if (digitCount % 2 != 0) continue;
+
+        BigNumber extractedNumber = extractNumber(i, digitCount);
+        BigNumber rebuiltNumber = rebuildNumber(extractedNumber, digitCount);
+        BigNumber combinedNumber = rebuiltNumber + extractedNumber;
+
+        if (i == combinedNumber) invalidIdCount+=i;
     }
     return invalidIdCount;
 }
 
-int main() {
+char *getFileContents(char* relativePath) {
     FILE* file = fopen(relativePath, "r");
     if (file == NULL) {
         raiseError("File not found");
@@ -54,32 +71,41 @@ int main() {
     // get file size to create a buffer of the right size
     fseek(file, 0, SEEK_END);
     int fileSize = ftell(file);
+
+    // reset to start of file
     rewind(file);
+
+    // initialise buffer of correct size and read data into file
     char *fileContents = (char *)malloc(fileSize * sizeof(char));
     int x = fread(fileContents, sizeof(char), fileSize, file);
+    // buffer must be null terminated!
     fileContents[x] = '\0';
 
+    return fileContents;
+}
+
+int main() {
+    char *fileContents = getFileContents(relativePath);
+
+    // strtok uses an internal state to track how it's split a string
+    // as it's being used to split two strings, we need to use strtok_r and provide our own state variable for each
     char *state1, *state2;
     char* numberPairs = strtok_r(fileContents, &setSeparator, &state1);
 
     // Buffer to store each line of the file.
-    unsigned long long int invalidIdCount = 0;
+    BigNumber invalidIdCount = 0;
     char *pair_buffer[2];
 
     while (numberPairs != NULL) {
         pair_buffer[0] = strtok_r(numberPairs, &pairSeparator, &state2);
         pair_buffer[1] = strtok_r(NULL, &pairSeparator, &state2);
         printf("----------------\n");
-        invalidIdCount += getInvalidIdCount(pair_buffer);
+        BigNumber idCount = getInvalidIdCount(pair_buffer);
+        invalidIdCount += idCount;//getInvalidIdCount(pair_buffer);
         printf("%s - %s\n", pair_buffer[0], pair_buffer[1]);
+        printf("idCount = %llu\n", idCount);
         printf("InvalidIdCount: %llu\n", invalidIdCount);
         numberPairs = strtok_r(NULL, &setSeparator, &state1);
     }
-
-    printf("----------------\n");
-    invalidIdCount += getInvalidIdCount(pair_buffer);
-    printf("%s - %s\n", pair_buffer[0], pair_buffer[1]);
-    printf("InvalidIdCount: %llu\n", invalidIdCount);
-
     return 0;
 }
