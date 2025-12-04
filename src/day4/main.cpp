e73ca8bfb46dfcbbd4058637df7946b5f38e2a19
teddy <teddyputus1@gmail.com>
2025-12-04 15:01:26 +0000

    WIP refactor

--- src/day4/main.cpp
+++ src/day4/main.cpp
@@ -14,8 +14,17 @@ enum MODE {ONCE, FOREVER};
 constexpr MODE RUN_MODE = ONCE; // part 1
 // constexpr MODE RUN_MODE = FOREVER; // part 2
 
+enum HORIZONTAL {LEFT=-1, H_CENTRE=0, RIGHT=1};
+enum VERTICAL {UP=-1, V_CENTRE=0, DOWN=1};
 
 std::vector<std::string> fileInput;
+
+bool checkChar(const int column, const int row, const VERTICAL vertical, const HORIZONTAL horizontal) {
+    const bool canHorizontal = 0 <= column + horizontal && column + horizontal <= fileInput[0].size();
+    const bool canVertical = 0 <= row + vertical && row + vertical <= fileInput.size()-1;
+
+    return canHorizontal && canVertical && fileInput[row+vertical][column+horizontal] == ROLL;
+}
 
 bool whileCondition(const int loopCount, const MODE runMode) {
     if (runMode == FOREVER) {
@@ -33,6 +42,23 @@ void loadFile() {
     }
 }
 
+int getAdjacentRolls(const int column, const int row) {
+    return  checkChar(column, row, UP, H_CENTRE) +
+            checkChar(column, row, DOWN, H_CENTRE) +
+            checkChar(column, row, V_CENTRE, RIGHT) +
+            checkChar(column, row, V_CENTRE, LEFT) +
+            checkChar(column, row, UP, RIGHT) +
+            checkChar(column, row, UP, LEFT) +
+            checkChar(column, row, DOWN, RIGHT) +
+            checkChar(column, row, DOWN, LEFT);
+}
+
+void markSpot(const MODE runMode, const int row, const int column) {
+    if (runMode == FOREVER) {
+        fileInput[row][column] = MARKED;
+    }
+}
+
 int main() {
     int totalAccessible = 0;
     int loopCount = 0;
@@ -41,41 +67,26 @@ int main() {
 
     while (whileCondition(loopCount, RUN_MODE)) {
         int numberRemoved = 0;
-        for(std::size_t i = 0; i < fileInput.size(); ++i) {
-            const bool canLookUp = i > 0;
-            const bool canLookDown = i < fileInput.size() - 1;
+        for(std::size_t row = 0; row < fileInput.size(); ++row) {
 
-                for(std::size_t x = 0; x < fileInput[i].size(); x++) {
-                    if (fileInput[i][x] != ROLL) {
+                for(std::size_t column = 0; column < fileInput[row].size(); column++) {
+                    if (fileInput[row][column] != ROLL) {
                         continue;
                     }
 
-                    const bool canLookLeft = x > 0;
-                    const bool canLookRight = x < fileInput[i].size() - 1;
-
-                    const int adjacentRolls = (canLookUp && fileInput[i-1][x] == ROLL) +
-                        (canLookDown && fileInput[i+1][x] == ROLL) +
-                        (canLookRight && fileInput[i][x+1] == ROLL) +
-                        (canLookLeft && fileInput[i][x-1] == ROLL) +
-                        (canLookUp && canLookRight && fileInput[i-1][x+1] == ROLL) +
-                        (canLookUp && canLookLeft && fileInput[i-1][x-1] == ROLL) +
-                        (canLookDown && canLookRight && fileInput[i+1][x+1] == ROLL) +
-                        (canLookDown && canLookLeft && fileInput[i+1][x-1] == ROLL);
-
+                    const int adjacentRolls = getAdjacentRolls(column, row);
 
                     if (adjacentRolls <= MAX_ADJACENT) {
-                        if (RUN_MODE == FOREVER) {
-                            fileInput[i][x] = MARKED;
-                        }
-
+                        markSpot(RUN_MODE, row, column);
                         totalAccessible += 1;
                         numberRemoved += 1;
                     }
                 }
-                std::cout << fileInput[i] << "\n";
+                std::cout << fileInput[row] << "\n";
         }
         std::cout << "Number removed: " << numberRemoved << "\n";
         if (numberRemoved == 0) {
+            // Nothing left to remove (specific to part 2)
             break;
         }
         loopCount++;
