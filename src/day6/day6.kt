6eceef450d06636663b9a6c04ad9e93a6aa2735b
teddy <teddyputus1@gmail.com>
2025-12-06 10:57:45 +0000

    Day 6 Part 2

--- src/day6/day6.kt
+++ src/day6/day6.kt
@@ -1,21 +1,21 @@
 import java.io.File
 
-class Arithmetic(operator: String) {
+class Arithmetic(operator: String){
     var numbers: List<Int> = mutableListOf<Int>();
     var total: Long = 0;
     var operator: String =
         when (operator) {
-            "+" -> operator;
-            "*" -> operator;
-            else -> {
-                println("Invalid OP $operator");
-                throw ArithmeticException("Invalid operator $operator");
-            }
-        }
+                        "+" -> operator;
+                        "*" -> operator;
+                        else -> {
+                            println("Invalid OP $operator");
+                            throw ArithmeticException("Invalid operator $operator");
+                            }
+                        }
 
-    fun appendNumber(newNumber: Int) {
+    fun appendNumber(newNumber: Int){
         numbers += newNumber;
-        if (numbers.size == 1) total = newNumber.toLong();
+        if(numbers.size == 1) total = newNumber.toLong();
         else {
             total = when (operator) {
                 "+" -> total + newNumber;
@@ -27,7 +27,7 @@ class Arithmetic(operator: String) {
             }
         }
     }
-}
+    }
 
 private fun Iterable<Arithmetic>.sumTotals(): Long {
     var sum: Long = 0;
@@ -40,8 +40,7 @@ private fun Iterable<Arithmetic>.sumTotals(): Long {
 fun readFileAsLinesUsingUseLines(fileName: String): List<String>
         = File(fileName).useLines { it.toList() }
 
-fun main() {
-    val input: List<String> = readFileAsLinesUsingUseLines(filepath);
+fun partOne(input: List<String> ){
     var operations: List<Arithmetic> = mutableListOf<Arithmetic>();
 
     for((lineIndex, line) in input.reversed().withIndex()){
@@ -55,4 +54,53 @@ fun main() {
         }
     }
     println("PART 1: ${operations.sumTotals()}");
+}
+
+fun getVerticalSlice(input: List<String>, index: Int ) : String{
+    val result = StringBuilder();
+
+    for(line in input){
+        result.append(line.getOrElse(index, { ' ' }))
+    }
+    return result.toString();
+}
+
+fun getLongestLineLength(input: List<String>) : Int{
+    var longestLine = 0;
+    for(line in input){
+        if(line.length > longestLine){
+            longestLine = line.length;
+        }
+    }
+    return longestLine;
+}
+
+fun partTwo(input: List<String> ){
+    var operations: List<Arithmetic> = mutableListOf<Arithmetic>();
+    val lineLength = getLongestLineLength(input);
+
+    var currentArithmetic = Arithmetic("+");
+
+    for(index in 0 until lineLength + 1){
+        var slice = getVerticalSlice(input, index);
+        var operator = slice.last().toString();
+        slice = slice.dropLast(1);
+
+        if(operator == "+" || operator == "*"){
+            currentArithmetic = Arithmetic(operator);
+        }
+        if(slice.trim().length == 0){
+            operations += currentArithmetic;
+        }else{
+            currentArithmetic.appendNumber(slice.trim().toInt());
+        }
+    }
+    println("PART 2: ${operations.sumTotals()}");
+}
+
+fun main() {
+    val filepath = "YOUR_FILEPATH_HERE"
+    val input: List<String> = readFileAsLinesUsingUseLines(filepath);
+    partOne(input);
+    partTwo(input);
 }
