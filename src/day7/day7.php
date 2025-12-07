55019ae62421ddd36808bf329c05befb2591a527
teddy <teddyputus1@gmail.com>
2025-12-07 11:24:26 +0000

    WIP Day 7 Part 1
    I actually don't know why this works but it does
    Also requires huge refactor

--- src/day7/day7.php
+++ src/day7/day7.php
@@ -1,5 +1,5 @@
 <?php
-$fp = "../../input_files/day_7_example.txt";
+$fp = "../../input_files/day_7.txt";
 
 $START_CHAR = "S";
 $BEAM_SPLITTER = "^";
@@ -20,11 +20,65 @@ $lineCount = 1;
 while (($line = fgets($file)) !== false) {
     echo "-------------------------\n";
    $beamSplitters = getLocations($line, $lineCount);
-   $splitCounter += count($beamSplitters);
-   $lineCount++;
+   // check if there is any at all
+    if(count($beamSplitters) == 0){
+//        echo "No beam splitters on line $lineCount\n";
+        if($lineCount == 1){
+//            echo "Setting start $startLocation[0]\n";
+            $beamSet[] = $startLocation[0];
+            $line[$startLocation[0]] = $BEAM;
+        }else{
+//            for($i = 0; $i < count($beamSet); $i++){
+//                $line[$beamSet[$i]] = $BEAM;
+//            }
+        }
+    }else{
+        $lineUnique = 0;
+        for($i = 0; $i < count($beamSplitters); $i++){
+            $splitterUnique = 0;
+            $beamX = $beamSplitters[$i];
+            if(in_array($beamX, $beamSet)){
+                if (($key = array_search($beamX, $beamSet)) !== false) {
+                    unset($beamSet[$key]);
+                }
+//                if(in_array($beamX+2, $beamSplitters) && in_array($beamX-2, $beamSplitters)){
+//                    continue;
+//                }
+                if($beamX > 0){
+//                    if(!in_array($beamX - 1, $beamSet)){
+//                        $splitterUnique += 1;
+//                    }
+                    $splitterUnique += 1;
+                    $beamSet[] = $beamX - 1;
+                    $line[$beamX - 1] = $BEAM;
+                }
+                if($beamX < strlen($line) - 1){
+//                    if(!in_array($beamX + 1, $beamSet)){
+//                        $splitterUnique += 1;
+//                    }
+                    $splitterUnique += 1;
+                    $beamSet[] = $beamX + 1;
+                    $line[$beamX + 1] = $BEAM;
+                }
+                if($splitterUnique > 0){
+                    echo "Unique at $beamX, $lineCount\n";
+                    $uniqueSplitCounter += 1;
+                }
+            }
+        }
+//        if($lineUnique % 2 == 0){
+//            echo "All paths unique on line $lineCount\n";
+//        }else{
+//            echo "Some overlap on line $lineCount\n";
+//        }
+//        $uniqueSplitCounter += $lineUnique;
+    }
+    $beamSet = array_unique($beamSet);
+//    echo $line;
+    $lineCount++;
 }
-$result = $splitCounter -1;
-echo "Result: $result\n";
+$result = count($beamSet);
+echo "Result: $uniqueSplitCounter\n";
 fclose($file);
 
 function getLocations($line, $row){
@@ -33,7 +87,7 @@ function getLocations($line, $row){
     $positions = array();
 
     while (($column = strpos($line, $BEAM_SPLITTER, $column))!== false) {
-        $positions[] = [$column, $row];
+        $positions[] = $column;
         $column = $column + strlen($BEAM_SPLITTER);
 //        echo "$column, ";
     }
